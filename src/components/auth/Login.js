9fdeeab9b06560f7e3cc88f6c2ac94f386566773
Cesar <cjhd92@gmail.com>
2022-06-02 14:36:32 -0400

    conmit rama main1

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -30,7 +30,7 @@ export const Login = () => {
         <div className='form-usuario'>
             <div className='contenedor-form sombra-dark'>
 
-                <h1>Iniciar Sesion1234RamaPrueba</h1>
+                <h1>Iniciar Sesion1234RamaMain</h1>
 
                 <form
                          onSubmit={onSubmit}>
